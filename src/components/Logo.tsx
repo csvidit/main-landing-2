9f5b2e31aba921921baf1be31d5267a109dd00a2
Vidit Khandelwal <viditk17@gmail.com>
2023-02-08 12:48:09 -0500

    logo size

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,9 +5,9 @@ const Logo = () => {
   return (
     <React.Fragment>
       <motion.svg
-        className="main-v v-anim "
-        width="200"
-        height="200"
+        className="main-v v-anim w-[100px] h-[100px] lg:w-[200px] lg:h-[200px]"
+        // width="200"
+        // height="200"
         viewBox="0 0 1835 2315"
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
