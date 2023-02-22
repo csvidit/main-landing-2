33e1dba89071dc788cd6c42fcfdb250a24d0eccb
Vidit Khandelwal <viditk17@gmail.com>
2023-02-22 16:53:29 -0500

    added subtle background

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -3,7 +3,7 @@ import styles from "./Main.module.css"
 
 const MainContainer = (props: {children: any}) =>
 {
-    return (<main className={"w-screen h-screen flex flex-col bg-black items-center "+styles.clash}>{props.children}</main>);
+    return (<main className={"w-screen h-screen flex flex-col items-center "+styles.main_container}>{props.children}</main>);
 }
 
 export default MainContainer;
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MainContent = (props: {children: any}) =>
 {
-    return (<div className="w-screen h-screen flex flex-col lg:flex-row lg:space-x-5 space-y-5 bg-black justify-center items-center">{props.children}</div>);
+    return (<div className="w-screen h-screen flex flex-col lg:flex-row lg:space-x-5 space-y-5 bg-transparent justify-center items-center">{props.children}</div>);
 }
 
 export default MainContent;
